a02a7382d8b4a43ae0b4fd833c59516fcd3568a9
EC2 Default User <ec2-user@ip-172-31-38-157.us-east-2.compute.internal>
2018-03-06 18:02:41 +0000

    Done.

---
+++ app/helpers/preferences_helper.rb
@@ -0,0 +1,14 @@
+module PreferencesHelper
+        
+    def artist_sort_order
+    end
+    
+    def song_sort_order
+    end
+    
+    def allow_create_artists
+    end
+    
+    def allow_create_songs
+    end
+end
